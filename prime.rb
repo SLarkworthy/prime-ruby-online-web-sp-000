14235258f407f408f7a0b2c52b54ec07bf4bb304
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-30 00:33:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 #let's try an inefficient but intuitive way first
 
-def prime?(n)
-  remainders = []
-  if n <= 1
-   false
-  elsif n > 1
-    (2..n-1).each do |num|
-      remainders << n % num
-    end
-    remainders.none? {|r| r == 0}
-  end
-end
+# def prime?(n)
+#   remainders = []
+#   if n <= 1
+#   false
+#   elsif n > 1
+#     (2..n-1).each do |num|
+#       remainders << n % num
+#     end
+#     remainders.none? {|r| r == 0}
+#   end
+# end
